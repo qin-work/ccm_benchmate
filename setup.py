558304cb0d34b4ae1deb070b2e106405ed56ff88
alper <celikalper84@gmail.com>
2025-03-31 15:37:46 -0400

    still adding files very broken and WIP

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ccm_demo',
+    version='0.1.0',
+    description="Python package for demonstrating simple proteomics integrations and one liners",
+    author='Alper Celik',
+    author_email='alper.celik@sickkids.ca',
+    packages=find_packages(),
+    zip_safe=False,
+    package_data={"": ["*.json"]},
+    include_package_data=True
+)
